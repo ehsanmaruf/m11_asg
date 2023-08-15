317ef6744674d71aca76b9db95b0b74c45241e8f
maruf <ehsanmaruf489@gmail.com>
2023-08-15 22:51:43 +0600

    data folder added

--- app/blog/page.js
+++ app/blog/page.js
@@ -10,7 +10,7 @@ const Blog = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const response = await fetch("data/blogData.json");
             
         const data = await response.json();
         console.log(data);
